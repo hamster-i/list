c1f1220cb069dc7f9072ed4af555f8e348c3f3aa
hamster-i <skuter@list.ru>
2026-09-22 23:11:05 +0400

    Update list.h

--- src/list.h
+++ src/list.h
@@ -62,8 +62,10 @@ typedef enum
 /// @brief  Функции для работы с односвязным списком
 /// @details В разработке...
 ///@{
+	
 LIST_RESULT LIST_AddNode(LIST* fList, LIST_TYPE_DATA fType, const void* fData);
 LIST_RESULT LIST_Clear(LIST* fList);
+
 /// @}
 
 #endif /* LIST_H_ */
